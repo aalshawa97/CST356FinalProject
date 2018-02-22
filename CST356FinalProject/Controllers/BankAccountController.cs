8cf11649a4653e23621a9562d0ed2be381764e0d
aalshawa97 <abdullahalshawa@gmail.com>
2018-02-22 13:52:34 -0800

    Added Methods To Bank Account Controller

--- CST356FinalProject/Controllers/BankAccountController.cs
+++ CST356FinalProject/Controllers/BankAccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CST356FinalProject.Data.Entities;
 using CST356FinalProject.Models;
 using CST356FinalProject.Models.View;
 
@@ -30,153 +31,142 @@ namespace CST356FinalProject.Controllers
 
         foreach (var bankAccount in bankAccounts)
         {
-          //var bankAccountViewModel = MapToBankAccountViewModel(bankAccount);
+          var bankAccountViewModel = MapToBankAccountViewModel(bankAccount);
           //bankAccountViewModels.Add(bankAccountViewModel);
         }
         return bankAccountViewModels;
       }
-
-      /*
-        [HttpGet]
-        public ActionResult Create(int userId)
+      private BankAccount MapToBankAccount(BankAccountViewModel bankAccountViewModel)
+      {
+        return new BankAccount
         {
-          ViewBag.UserId = userId;
+          Id = bankAccountViewModel.Id,
+          AccountNumber = bankAccountViewModel.AccountNumber,
+          UserId = bankAccountViewModel.UserId,
+          AccountType = bankAccountViewModel.AccountType,
+        };
+      }
 
-          return View();
+      private BankAccountViewModel MapToBankAccountViewModel(BankAccount bankAccount)
+      { 
+        return new BankAccountViewModel
+        {
+          Id = bankAccount.Id,
+          AccountNumber = bankAccount.AccountNumber,
+          UserId = bankAccount.UserId,
+          AccountType = bankAccount.AccountType,
+        };
+      }
+
+      [HttpGet]
+      public ActionResult Create(int userId)
+      {
+        ViewBag.UserId = userId;
+
+        return View();
+      }
+
+      [HttpPost]
+      public ActionResult Create(BankAccountViewModel bankAccountViewModel)
+      {
+        if (ModelState.IsValid)
+        {
+          Save(bankAccountViewModel);
+          return RedirectToAction("List", new { bankAccountViewModel.UserId });
         }
 
-        [HttpPost]
-        public ActionResult Create(PetViewModel petViewModel)
-        {
-          if (ModelState.IsValid)
-          {
-            Save(petViewModel);
-            return RedirectToAction("List", new { petViewModel.UserId });
-          }
+        return View();
+      }
 
-          return View();
+      [HttpGet]
+      public ActionResult Edit(int id)
+      {
+        var bankAccount = GetBankAccount(id);
+
+        return View(bankAccount);
+      }
+
+      private BankAccountViewModel GetBankAccount(int bankAccountId)
+      {
+        var dbContext = new ADbContext();
+
+        var bankAccount = dbContext.BankAccounts.Find(bankAccountId);
+
+        return MapToBankAccountViewModel(bankAccount);
+      }
+
+      private void Save(BankAccountViewModel bankAccountViewModel)
+      {
+        var dbContext = new ADbContext();
+
+        var bankAccount = MapToBankAccount(bankAccountViewModel);
+
+        dbContext.BankAccounts.Add(bankAccount);
+
+        dbContext.SaveChanges();
+      }
+
+
+
+      [HttpPost]
+      public ActionResult Edit(BankAccountViewModel bankAccountViewModel)
+      {
+        bankAccountViewModel.UserId = GetBankAccount(bankAccountViewModel.Id).UserId;
+
+        if (ModelState.IsValid)
+        {
+          UpdateBankAccount(bankAccountViewModel);
+
+          return RedirectToAction("List", new { bankAccountViewModel.UserId });
         }
 
-        [HttpGet]
-        public ActionResult Edit(int id)
+        return View();
+      }
+
+      private void UpdateBankAccount (BankAccountViewModel bankAccountViewModel)
+      {
+        var dbContext = new ADbContext();
+
+        var bankAccount = dbContext.BankAccounts.Find(bankAccountViewModel.Id);
+
+        CopyToBankAccount(bankAccountViewModel, bankAccount);
+
+        dbContext.SaveChanges();
+      }
+
+      private void CopyToBankAccount(BankAccountViewModel bankAccountViewModel, BankAccount bankAccount)
+      {
+        bankAccount.AccountNumber = bankAccountViewModel.AccountNumber;
+        bankAccount.AccountNumber = bankAccountViewModel.AccountNumber;
+        bankAccount.AccountType = bankAccountViewModel.AccountType;
+      }
+
+      public ActionResult Details(int id)
+      {
+        var bankAccount = GetBankAccount(id);
+
+        return View(bankAccount);
+      }
+
+      public ActionResult Delete(int id)
+      {
+        var bankAccount = GetBankAccount(id);
+
+        DeleteBankAccount(id);
+
+        return RedirectToAction("List", new { UserId = bankAccount.UserId });
+      }
+      private void DeleteBankAccount(int id)
+      {
+        var dbContext = new ADbContext();
+
+        var bankAccount = dbContext.BankAccounts.Find(id);
+
+        if (bankAccount != null)
         {
-          var pet = GetPet(id);
-
-          return View(pet);
-        }
-
-        [HttpPost]
-        public ActionResult Edit(PetViewModel petViewModel)
-        {
-          petViewModel.UserId = GetPet(petViewModel.Id).UserId;
-
-          if (ModelState.IsValid)
-          {
-            UpdatePet(petViewModel);
-
-            return RedirectToAction("List", new { petViewModel.UserId });
-          }
-
-          return View();
-        }
-
-        public ActionResult Details(int id)
-        {
-          var pet = GetPet(id);
-
-          return View(pet);
-        }
-
-        private void UpdatePet(PetViewModel petViewModel)
-        {
-          var dbContext = new AppDbContext();
-
-          var pet = dbContext.Pets.Find(petViewModel.Id);
-
-          CopyToPet(petViewModel, pet);
-
+          dbContext.BankAccounts.Remove(bankAccount);
           dbContext.SaveChanges();
         }
-
-        private void CopyToPet(PetViewModel petViewModel, Pet pet)
-        {
-          pet.Name = petViewModel.Name;
-          pet.Age = petViewModel.Age;
-          pet.NextCheckup = petViewModel.NextCheckup;
-          pet.VetName = petViewModel.VetName;
-          pet.UserId = petViewModel.UserId;
-        }
-
-        public ActionResult Delete(int id)
-        {
-          var pet = GetPet(id);
-
-          DeletePet(id);
-
-          return RedirectToAction("List", new { UserId = pet.UserId });
-        }
-
-        private PetViewModel GetPet(int petId)
-        {
-          var dbContext = new AppDbContext();
-
-          var pet = dbContext.Pets.Find(petId);
-
-          return MapToPetViewModel(pet);
-        }
-
-          return petViewModels;
-        }
-
-        private void Save(PetViewModel petViewModel)
-        {
-          var dbContext = new AppDbContext();
-
-          var pet = MapToPet(petViewModel);
-
-          dbContext.Pets.Add(pet);
-
-          dbContext.SaveChanges();
-        }
-
-        private void DeletePet(int id)
-        {
-          var dbContext = new AppDbContext();
-
-          var pet = dbContext.Pets.Find(id);
-
-          if (pet != null)
-          {
-            dbContext.Pets.Remove(pet);
-            dbContext.SaveChanges();
-          }
-        }
-
-        private Pet MapToPet(PetViewModel petViewModel)
-        {
-          return new Pet
-          {
-            Id = petViewModel.Id,
-            Name = petViewModel.Name,
-            Age = petViewModel.Age,
-            NextCheckup = petViewModel.NextCheckup,
-            VetName = petViewModel.VetName,
-            UserId = petViewModel.UserId
-          };
-        }
-
-        private PetViewModel MapToPetViewModel(Pet pet)
-        {
-          return new PetViewModel
-          {
-            Id = pet.Id,
-            Name = pet.Name,
-            Age = pet.Age,
-            NextCheckup = pet.NextCheckup,
-            VetName = pet.VetName,
-            UserId = pet.UserId
-          };
-        }
-      */
-    }
+      }
+  }
 }
