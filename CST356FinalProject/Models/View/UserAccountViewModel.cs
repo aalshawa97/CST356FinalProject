4f5ac93afd62402dd27f35df49ad929f45a2149c
aalshawa97 <abdullahalshawa@gmail.com>
2018-02-25 17:11:18 -0800

    Added User Account View Model

---
+++ CST356FinalProject/Models/View/UserAccountViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CST356FinalProject.Models.View
+{
+  public class UserAccountViewModel
+  {
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name = "User Name")]
+    public string Username { get; set; }
+
+    [Required]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+  }
+}
